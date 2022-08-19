cb2efba4d50b28e8c7bb78623a13605aaa4a0ba5
mohit <mohit11sps@gmail.com>
2022-08-19 18:45:39 +0530

    my first comment

---
+++ layout/css/custom.js
@@ -0,0 +1,13 @@
+// Basic initialization is like this:
+// $('.your-class').slick();
+
+// I added some other properties to customize my slider
+// Play around with the numbers and stuff to see
+// how it works.
+
+
+$('banner').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1
+});
