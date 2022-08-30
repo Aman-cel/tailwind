6237195eac3bb6111cc0275c9f2d7980bd1c21be
Aman-cel <aman_sharma1@softprodigy.com>
2022-08-30 18:52:43 +0530

    design page

--- layout/css/custom.js
+++ layout/css/custom.js
@@ -1,13 +1,58 @@
-// Basic initialization is like this:
-// $('.your-class').slick();
+/*banner slider jquery */
 
-// I added some other properties to customize my slider
-// Play around with the numbers and stuff to see
-// how it works.
-
-
-$('banner').slick({
+$('#banner_slider').slick({
   infinite: true,
   slidesToShow: 1,
-  slidesToScroll: 1
+  slidesToScroll: 1,
+dots: true,
+  speed:1000,
+  arrows: false,
+  autoplay:true
+
 });
+
+
+
+
+
+/*       why choose us counter jquery */  
+jQuery(document).ready(function( $ ) {
+$('.counter').counterUp({
+ delay: 30,
+ time: 1000
+});
+});
+
+
+
+
+// toogle class method forphone
+
+$(".toggle_bars").click(function(){
+$("body").toggleClass("opennav");
+});
+
+
+
+
+
+$('.service_slider').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  dots: false,
+draggable: false,
+  arrows: false,
+  autoplay: true,
+autoplaySpeed: 1000,
+
+});
+
+
+
+
+
+
+
+
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,38 @@ module.exports = {
     './layout/*.html'
   ],
   theme: {
-    extend: {},
+    colors: {
+      'blue': '#1fb6ff',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+    },
+    
   },
   plugins: [
 
-    require('@tailwindcss/typography')
+    require('@tailwindcss/typography'),
+  
+    
   ],
+}
+
+module.exports = {
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'purple': '#3f3cbb',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+    },
+  },
 }
